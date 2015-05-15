e5601dbf83a5a72b8536ab5c24c20341dd4a3207
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-15 11:18:26 -0400

    Enables to pass test_employees_can_receive_reviews.

--- employee.rb
+++ employee.rb
@@ -1,13 +1,19 @@
 class Employee
 
-  attr_reader :name, :email, :phone, :pay
+  attr_reader :name, :email, :phone, :pay, :review
 
   def initialize(name, email, phone, pay)
     @name = name
     @email = email
     @phone = phone.to_i
     @pay = pay.to_f
+    @review = ""
   end
+
+  def add_review(text)
+    @review = text
+  end
+
 
 #array.map &:name
 #assert_in_delta 129.95/3, the_price.price, 0.01  asserts the answers = within the range of 1 cent
