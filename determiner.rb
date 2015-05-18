c720fe0e42fbe558ac84d5c9e6f101f9bfec91ba
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-18 02:11:18 -0400

    Add a class to analyze text data and classify it.

--- determiner.rb
+++ determiner.rb
@@ -1,13 +1,28 @@
 class Determiner
   attr_reader :result
 
-  def initialize
+  def initialize(review)
+    @review = review.split(/\./)
     @good = 0
     @bad = 0
-    @result
+    @undefined = 0
+    @result = ''
+    @negative = ["concern", "off", "bad", "poor", "inconsistent", "dissatisfied",
+                 "less", "limitation", "interrupt", "disagree", "nasty",
+                 "unacceptable", "awful", "innapropriate"]
+    @positive = ["good", "great", "alright", "well", "acceptable", "admirable",
+                 "asset", "help", "Happy", "effective", "consistent", "satisfied",
+                 "wonderful", "fantastic", "excellent", "amazing", "awsome",
+                 "astonishing", "pleasure", "successfull", "enjoy", "devoted",
+                 "perfect"]
   end
 
-  def analyze(text)
+  def analyze
+    @review.each do |x|
+      @negative.each {|a| x.match(a) ? @bad += 1 : @undefined += 1}
+      @positive.each {|b| x.match(b) ? @good += 1 : @undefined += 1}
+    end
+    @bad > @good ? "poor" : "satisfactory"
   end
 
 end
